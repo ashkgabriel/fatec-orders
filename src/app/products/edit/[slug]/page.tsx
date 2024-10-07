7e23385e43726c5b696d88b8227364c1504ca25b
Gabriel Fortes Ashikaga <gabriel.ashikaga@fatec.sp.gov.br>
2024-10-07 19:58:35 -0300

    branch da aula 07-10

--- src/app/products/edit/[slug]/page.tsx
+++ src/app/products/edit/[slug]/page.tsx
@@ -1,13 +1,38 @@
 "use client";
 import EditTemplate from "@/components/templates/products/EditTemplate";
-import React from "react";
+import { env } from "@/config/env";
+import { IProduct } from "@/interfaces/IProduct";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 interface ProductEditProps {
   params: { slug: string };
 }
 
 const ProductEdit: React.FC<ProductEditProps> = ({ params }) => {
+  const [product, setProduct] = useState<IProduct>()
   
+  // Feito em aula
+  useEffect(() => {
+    const fetchData = async () => {
+      if(!params.slug) return;
+      const response = await axios.get(`${env.apiBaseUrl}/produto/${params.slug}`)
+
+      const { id, descricao, marca, peso_gramas, sabor } = response.data.produto
+      
+      setProduct({
+        brand,
+        description,
+        flavor,
+        value,
+        
+      })
+
+      console.log(response.data.produto);
+    }
+    fetchData()
+  }, [params.slug] )
+
   return <EditTemplate/>
 };
 
